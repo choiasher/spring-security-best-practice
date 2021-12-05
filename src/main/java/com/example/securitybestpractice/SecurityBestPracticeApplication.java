5ad0900e9dd0df5962e7dc4fc5b9e45316e9b52a
= <tahun970@gmail.com>
2021-12-05 15:19:20 +0900

    init project

---
+++ src/main/java/com/example/securitybestpractice/SecurityBestPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitybestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBestPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityBestPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/securitybestpractice/SecurityBestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securitybestpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityBestPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
